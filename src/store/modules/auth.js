e220f5ee546e459aae924d4d5fa8c9f1fc8c13ec
voitd <dimytr.voitenko@gmail.com>
2021-02-04 22:39:11 +0200

    chore: init repos

---
+++ src/store/modules/auth.js
@@ -0,0 +1,23 @@
+const state = {
+  isSubmitting: false,
+}
+
+const mutations = {
+  registerStart(state) {
+    state.isSubmitting = true
+  },
+}
+
+const actions = {
+  register(context) {
+    setTimeout(() => {
+      context.commit('registerStart')
+    }, 1000)
+  },
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+}
---
+++ src/views/Register.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{ name: 'login' }">Need an account?</router-link>
+          </p>
+          VALIDATIONS ERROR
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input class="form-control" type="text" placeholder="Username" />
+            </fieldset>
+            <fieldset class="form-group">
+              <input class="form-control" type="text" placeholder="Email" />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control"
+                type="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg pull-xs-right" :disabled="isSubmitting">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvRegister',
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    },
+  },
+  methods: {
+    onSubmit() {
+      console.log('submited!')
+      this.$store.dispatch('register')
+    },
+  },
+}
+</script>
